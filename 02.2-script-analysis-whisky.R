fd90f8962e17dc239c7c44e8077f06894ede7263
Luis Auza <luis.auza@gmail.com>
2023-09-02 13:51:12 +0000

    WIP

--- 02.2-script-analysis-whisky.R
+++ 02.2-script-analysis-whisky.R
@@ -10,3 +10,22 @@ DF3 <- DF3 %>% as_tibble()
 # Generando gráficos ----
 
 ## Marca que más compra (lealtad)
+
+tabla_lealtad <- DF3 %>% 
+  # contar los casos
+  count(`¿Cuál es la marca que más compra?`) %>% 
+  # ordenar la tabla
+  arrange(desc(n)) %>% 
+  # calcular los %
+  mutate(proporción = n/sum(n),
+         porcentaje = scales::percent(proporción))
+
+
+## Exportado en formato Excel
+write.xlsx(x = tabla_lealtad,
+           file = "lealtad.xlsx")
+
+
+
+
+
