0924228203b14a1f5e0e5b57cf0ac4139e40bd59
yucolax <yucolax@gmail.com>
2023-09-09 00:25:59 +0000

    WIP

--- 02.2-script-analysis-whisky.R
+++ 02.2-script-analysis-whisky.R
@@ -27,5 +27,35 @@ write.xlsx(x = tabla_lealtad,
 
 
 
+## Grafico de columnas
 
+tabla_lealtad %>%
+  
+  # renombrando una columna
+  rename(Marca = `¿Cuál es la marca que más compra?`) %>%
+  
+  mutate(Marca = factor(Marca),
+         Marca = fct_reorder(Marca, n, .desc = T)) %>%
+  
+  ggplot(mapping = aes(x = Marca,
+                       y = proporción,
+                       fill =Marca,
+                       label = porcentaje)) +
+  geom_col() +
+  
+  geom_label(fill = "white") +
+  
+  theme_minimal() +
+  
+  labs(title = "Lealtad de marca",
+       subtitle = "Cuál es la marca que más compra?",
+       caption = "Johnnie Walker es la marca con mayor índice de lealtad") +
+  
+  scale_fill_viridis_d() +
+  
+  scale_y_continuous(labels = scales::percent) +
+  
+  theme(legend.position = "none",
+        panel.grid.major.x = element_blank(),
+        axis.title = element_blank())
 
